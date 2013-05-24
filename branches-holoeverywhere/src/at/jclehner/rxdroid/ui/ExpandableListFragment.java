bb50fa1fbc8a15720d4defb1e6596553331f7cd0
joseph.c.lehner@gmail.com <joseph.c.lehner@gmail.com@fe94c659-f860-bb42-7527-049cdc510943>
2013-05-24 14:29:39 +0000

    Branch for testing HoloEverywhere

---
+++ branches-holoeverywhere/src/at/jclehner/rxdroid/ui/ExpandableListFragment.java
@@ -0,0 +1,61 @@
+package at.jclehner.rxdroid.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import at.jclehner.rxdroid.R;
+import at.jclehner.rxdroid.Version;
+
+public class ExpandableListFragment extends Fragment
+{
+	private ExpandableListView mList;
+	private ExpandableListAdapter mAdapter;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		View v = inflater.inflate(R.layout.layout_expandable_list, null);
+		mList = ((ExpandableListView) v.findViewById(android.R.id.list));
+		mList.setAdapter(mAdapter);
+		return v;
+	}
+
+	public void setListAdapter(ExpandableListAdapter adapter)
+	{
+		mAdapter = adapter;
+
+		if(mList != null)
+			mList.setAdapter(adapter);
+	}
+
+	public ExpandableListAdapter getAdapter() {
+		return mAdapter;
+	}
+
+	public void expandAll(boolean animate)
+	{
+		if(mList == null || mAdapter == null)
+			return;
+
+		for(int i = 0; i != mAdapter.getGroupCount(); ++i)
+		{
+			if(Version.SDK_IS_JELLYBEAN_OR_NEWER)
+				mList.expandGroup(i, animate);
+			else
+				mList.expandGroup(i);
+		}
+	}
+
+	public void collapseAll()
+	{
+		if(mList == null || mAdapter == null)
+			return;
+
+		for(int i = 0; i != mAdapter.getGroupCount(); ++i)
+			mList.collapseGroup(i);
+	}
+}
